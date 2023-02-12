be4f59fda5f521de18c102dc5883dfe1c4c1fa8e
buniozak <ozak3465@gmail.com>
2023-02-12 08:03:46 +0300

    Adding Files

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__,template_folder="docs")
+
+
+
+@app.route("/")
+def index():
+    return render_template("ai_1.html")
+
+
+
+if __name__ == "__main__":
+    app.run()
